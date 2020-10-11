b5844a8b59a81a61b78fe9c46d211b72957e12ea
lucasmedeiros <lucasmed812@gmail.com>
2020-10-11 07:39:18 -0300

    Fix indentation

--- src/plot/fitness_plot.py
+++ src/plot/fitness_plot.py
@@ -5,49 +5,49 @@ from .plot_model import PlotModel
 
 
 class FitnessPlot:
-    def __init__(self, fitnesses):
-        self.fitnesses = fitnesses
+  def __init__(self, fitnesses):
+    self.fitnesses = fitnesses
 
-    """
-    Returns the statistical value for the population fitnesses, given the model.
-    If model is "MEAN", it returns the mean of the popoulation fitnesses.
-    If model is "MEDIAN", it returns the median of the popoulation fitnesses.
-    """
+  """
+  Returns the statistical value for the population fitnesses, given the model.
+  If model is "MEAN", it returns the mean of the popoulation fitnesses.
+  If model is "MEDIAN", it returns the median of the popoulation fitnesses.
+  """
 
-    def get_fitnesses_statistical_value(self, model):
-        results = []
-        calc_function = None
-        if model == PlotModel.MEAN:
-            calc_function = mean
-        elif model == PlotModel.MEDIAN:
-            calc_function = median
-        else:
-            raise Exception("Plot model required: 'MEDIAN' or 'MEAN'")
+  def get_fitnesses_statistical_value(self, model):
+    results = []
+    calc_function = None
+    if model == PlotModel.MEAN:
+      calc_function = mean
+    elif model == PlotModel.MEDIAN:
+      calc_function = median
+    else:
+      raise Exception("Plot model required: 'MEDIAN' or 'MEAN'")
 
-        for generation_fitnesses in self.fitnesses:
-            generation_statistical_value = calc_function(generation_fitnesses)
-            results.append(generation_statistical_value)
+    for generation_fitnesses in self.fitnesses:
+      generation_statistical_value = calc_function(generation_fitnesses)
+      results.append(generation_statistical_value)
 
-        return results
+    return results
 
-    """
-    Generates a graph of a-maze solver generations vs. population fitness
-    """
+  """
+  Generates a graph of a-maze solver generations vs. population fitness
+  """
 
-    def plot(self, model):
-        fitnesses = self.get_fitnesses_statistical_value(model)
-        generations = range(len(fitnesses))
+  def plot(self, model):
+    fitnesses = self.get_fitnesses_statistical_value(model)
+    generations = range(len(fitnesses))
 
-        data_dict = {"generations": generations, "fitnesses": fitnesses}
+    data_dict = {"generations": generations, "fitnesses": fitnesses}
 
-        pyplot.plot(
-            "generations",
-            "fitnesses",
-            data=data_dict,
-            color="green",
-            marker="o",
-            label="generations vs fitnesses",
-        )
-        pyplot.xticks(generations)
-        pyplot.yticks(fitnesses)
-        pyplot.show()
+    pyplot.plot(
+      "generations",
+      "fitnesses",
+      data=data_dict,
+      color="green",
+      marker="o",
+      label="generations vs fitnesses",
+    )
+    pyplot.xticks(generations)
+    pyplot.yticks(fitnesses)
+    pyplot.show()
--- src/plot/plot_model.py
+++ src/plot/plot_model.py
@@ -1,5 +1,5 @@
 from enum import Enum
 
 class PlotModel(Enum):
-    MEAN = 0
-    MEDIAN = 1
+  MEAN = 0
+  MEDIAN = 1
